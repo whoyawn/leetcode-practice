36fc1c1ec44b4426745e580daf9b5d0cacbdad2f
Huyanh Hoang <whoyawn@outlook.com>
2021-11-07 09:09:05 -0800

    Time: 4 ms (79.10%), Space: 14.1 MB (11.44%) - LeetHub

--- binary-tree-inorder-traversal/binary-tree-inorder-traversal.swift
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.swift
@@ -14,22 +14,48 @@
  * }
  */
 """
+left, root, right
 
+going down, add left until hit a null
 
+hit null -> pop back up stack, then go down another "stack" for the right side
+
+ 1
+2 3
+
+[2,1,3]
+[
 """
 class Solution {
-    func inorderTraversal(_ root: TreeNode?) -> [Int] {
-        var arr = [Int]()
-        traverse(root, &arr)
-        return arr
-    }
+//     func inorderTraversal(_ root: TreeNode?) -> [Int] {
+//         var arr = [Int]()
+//         traverse(root, &arr)
+//         return arr
+//     }
     
-    func traverse(_ node: TreeNode?, _ arr: inout [Int]) {
-        guard let node = node else {
-            return
+//     func traverse(_ node: TreeNode?, _ arr: inout [Int]) {
+//         guard let node = node else {
+//             return
+//         }
+//         traverse(node.left, &arr)
+//         arr.append(node.val)
+//         traverse(node.right, &arr)
+//     }
+    func inorderTraversal(_ root: TreeNode?) -> [Int] {
+        guard let root = root else { return [] }
+        var node: TreeNode? = root
+        var inorder: [Int] = []
+        var stack: [TreeNode] = []
+        while !stack.isEmpty || node != nil {
+            if node != nil {
+                stack.append(node!)
+                node = node!.left
+            } else {
+                let popped = stack.removeLast()
+                inorder.append(popped.val)
+                node = popped.right
+            }
         }
-        traverse(node.left, &arr)
-        arr.append(node.val)
-        traverse(node.right, &arr)
+        return inorder
     }
 }
