1499f206f7298a24618125609f8834d955be2438
Huyanh Hoang <whoyawn@outlook.com>
2021-11-11 17:17:50 -0800

    Time: 24 ms (93.04%), Space: 19.1 MB (46.09%) - LeetHub

--- path-sum-ii/path-sum-ii.swift
+++ path-sum-ii/path-sum-ii.swift
@@ -22,29 +22,48 @@ store a sum and a path at each node of the list
 """
 class Solution {
     func pathSum(_ root: TreeNode?, _ targetSum: Int) -> [[Int]] {
-        var stack: [(TreeNode, [Int], Int)] = []
         var paths: [[Int]] = []
-        var curPath: [Int] = []
-        var curSum = 0
-        var cur = root
-        var prev: TreeNode?
-        while !stack.isEmpty || cur != nil {
-            if cur != nil {
-                curSum += cur!.val
-                curPath.append(cur!.val)
-                stack.append((cur!, curPath, curSum))
-                if cur!.left == nil && cur!.right == nil && curSum == targetSum {
-                    paths.append(curPath)
-                }
-                cur = cur!.left
-            } else {
-                let popped = stack.removeLast()
-                let node = popped.0
-                curSum = popped.2
-                curPath = popped.1
-                cur = node.right
-            }
-        }
+        findPath(root, [], &paths, targetSum)
         return paths
     }
+    
+    func findPath(_ root: TreeNode?, _ path: [Int], _ paths: inout [[Int]], _ targetSum: Int) {
+        guard let root = root else { return }
+        var path = path + [root.val]
+        if root.left == nil && root.right == nil && targetSum - root.val == 0 {
+            paths.append(path)
+            return
+        }
+        findPath(root.left, path, &paths, targetSum - root.val)
+        findPath(root.right, path, &paths, targetSum - root.val)
+    }
+    // func pathSum(_ root: TreeNode?, _ targetSum: Int) -> [[Int]] {
+    //     var stack: [(TreeNode, [Int], Int)] = []
+    //     var paths: [[Int]] = []
+    //     var curPath: [Int] = []
+    //     var curSum = 0
+    //     var cur = root
+    //     var prev: TreeNode?
+    //     while !stack.isEmpty || cur != nil {
+    //         if cur != nil {
+    //             curSum += cur!.val
+    //             curPath.append(cur!.val)
+    //             stack.append((cur!, curPath, curSum))
+    //             // if cur!.left == nil && cur!.right == nil && curSum == targetSum {
+    //             //     paths.append(curPath)
+    //             // }
+    //             cur = cur!.left
+    //         } else {
+    //             let popped = stack.removeLast()
+    //             if popped.0.left == nil && popped.0.right == nil && popped.2 == targetSum {
+    //                 paths.append(popped.1)
+    //             }
+    //             let node = popped.0
+    //             curSum = popped.2
+    //             curPath = popped.1
+    //             cur = node.right
+    //         }
+    //     }
+    //     return paths
+    // }
 }
