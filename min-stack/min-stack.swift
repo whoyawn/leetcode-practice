259af1a44d20b48c4ff00a6e9a0afe2758ae3e5c
Huyanh Hoang <whoyawn@outlook.com>
2021-11-20 12:45:38 -0800

    Time: 68 ms (75.29%), Space: 15.5 MB (19.54%) - LeetHub

--- min-stack/min-stack.swift
+++ min-stack/min-stack.swift
@@ -1,32 +1,72 @@
-
+// LinkedList
 class MinStack {
     
-    var minimums: [Int] = []
-    var stack: [Int] = []
+    class Node {
+        let val: Int
+        let min: Int
+        var prev: Node?
+        
+        init(_ val: Int,_ min: Int) {
+            self.val = val
+            self.min = min
+        }
+    }
+    
+    var last: Node? = Node(Int.max, Int.max)
 
     init() {
         
     }
-    // update min
+    //              p<-n,l
+    // update min [-2, 1]
     func push(_ val: Int) {
-        stack.append(val)
-        let lastMinimum = minimums.last ?? val
-        minimums.append(min(lastMinimum, val))
+        let node = Node(val, min(val, last?.min ?? val))
+        node.prev = last
+        last = node
     }
     // update new min
     func pop() {
-        minimums.removeLast()
-        stack.removeLast()
+        last = last?.prev
     }
     
     func top() -> Int {
-        stack.last ?? 0
+        last?.val ?? Int.max
     }
     
     func getMin() -> Int {
-        minimums.last ?? 0
+        last?.min ?? Int.max
     }
 }
+
+// Array implementation
+// class MinStack {
+    
+//     var minimums: [Int] = []
+//     var stack: [Int] = []
+
+//     init() {
+        
+//     }
+//     // update min
+//     func push(_ val: Int) {
+//         stack.append(val)
+//         let lastMinimum = minimums.last ?? val
+//         minimums.append(min(lastMinimum, val))
+//     }
+//     // update new min
+//     func pop() {
+//         minimums.removeLast()
+//         stack.removeLast()
+//     }
+    
+//     func top() -> Int {
+//         stack.last ?? 0
+//     }
+    
+//     func getMin() -> Int {
+//         minimums.last ?? 0
+//     }
+// }
 
 /**
  * Your MinStack object will be instantiated and called as such:
