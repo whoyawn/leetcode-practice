d2d477f22c02cb7e4cb71a4c024cd893f0263f39
Huyanh Hoang <whoyawn@outlook.com>
2022-01-21 12:27:03 -0800

    Time: 84 ms (38.20%), Space: 14.8 MB (79.78%) - LeetHub

---
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.swift
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func kthSmallest(_ root: TreeNode?, _ k: Int) -> Int {
+        var kth = -1
+        var call = k
+        func helper(_ root: TreeNode?) {
+            guard let root = root else { return }
+            helper(root.left)
+            call -= 1
+            if call == 0 { 
+                kth = root.val
+                return
+            }
+            helper(root.right)
+        }
+        helper(root)
+        return kth
+    }
+}
