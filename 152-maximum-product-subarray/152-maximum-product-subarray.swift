9aeba70ac30be4f42a50d82844c69f77d1002440
Huyanh Hoang <whoyawn@outlook.com>
2022-01-26 14:23:48 -0800

    Time: 32 ms (60.97%), Space: 14.5 MB (40.17%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.swift
+++ 152-maximum-product-subarray/152-maximum-product-subarray.swift
@@ -1,71 +1,26 @@
- """
-output: product
-
-largest product:
-
-product: positive: pos pos / neg neg
-nega: pos neg
-
-[1,2,3,4]
-[1,2,3,-4] -> [1,2,3]
-[-1,2,3,4] -> [2,3,4]
-[1,2,-3,4,5,6] -> [4,5,6]
-[1,2,-3,-4] -> [1,2,-3,-4]
-[1,2,-3,-4,5,6] -> [1,2,-3,-4,5,6]
-[1,2,-3,-4,5,-6] -> [1,2,-3,-4,5]
-[1,-2]
-[-1,2,3,-4,-5,6]
-
-if max pos * neg, that's smallest possible, min
-if max pos * pos -> maximum
-if max neg * neg -> maximum
-max neg * pos -> min
-
-adding a new number can only expand your local max
-
-0 throws away the maximum at that point, start over from 1 and compare to prev max
-
-[-2,2,-1,-6]
-
-1. loop through, build local maximum
-2. keep track of largest + number so far and largest - number so far
-3. multiply both in both times
-3.5 running product, reset to 1 whenever see 0
-4. at each point, largest pos = max(running * cur, currentMax)
-largest neg = min(running * cur, currentMin)
-   6
-     -12  24
-[2,3,-2,  4]
-ans = 6
-
-   6
-     -6 -24 48 
-[2,3,-2, 4, -2]
-
--1 -1 -1-1 
--1 -2
-[-1,2,3,4] -> [2,3,4]
-[2,-1] -> [2]
-[1,2,3,4]
-[2,3,-2,4,-2]
+"""
+[2,3,-2,-5,6,-1,4] -> 60
+[2,3,-2,-5,6,-10,10]
 
 """
 class Solution {
     func maxProduct(_ nums: [Int]) -> Int {
         guard !nums.isEmpty else { return -1 }
-        var globalMax = nums[0]
-        var localMax = 1
-        var localMin = 1
-        for num in nums {
-            let products = [localMax * num, localMin * num, num]
-            guard let potentialMax = products.max(),
-                 let potentialMin = products.min() else { return -1 }
-            localMax = potentialMax
-            localMin = potentialMin
-            
-            globalMax = max(globalMax, localMax)
+        var (pre, suf) = (1, 1)
+        var largestProduct = nums[0]
+        for (front, back) in zip(nums, nums.reversed()) {
+            largestProduct = max(largestProduct, pre * front, suf * back)
+            if front == 0 {
+                pre = 1
+            } else {
+                pre *= front
+            }
+            if back == 0 {
+                suf = 1
+            } else {
+                suf *= back
+            }
         }
-        
-        return globalMax
+        return largestProduct
     }
 }
