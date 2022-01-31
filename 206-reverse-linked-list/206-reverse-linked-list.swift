15cb6d9de85e69939f015423c28fdaa8fe0bfae1
Huyanh Hoang <whoyawn@outlook.com>
2022-01-31 14:27:34 -0800

    Time: 18 ms (37.15%), Space: 14.9 MB (40.71%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.swift
+++ 206-reverse-linked-list/206-reverse-linked-list.swift
@@ -9,27 +9,35 @@
  * }
  */
 """
-/->1->2->/ ... /->2->1->/
-1->/
-
-cur = 2, next = nil
-nil->next = 2
-return nil
+1->2->3
+1<-2<-3
 """
+// class Solution {
+//     func reverseList(_ head: ListNode?) -> ListNode? {
+//         var newHead: ListNode?
+//         func reverse(_ head: ListNode?) -> ListNode? {
+//             guard let cur = head else { return nil }
+//             if cur.next == nil {
+//                 newHead = cur
+//             }
+//             let next = reverse(cur.next)
+//             next?.next = cur
+//             return cur
+//         }
+//         reverse(head)?.next = nil
+
+//         return newHead
+//     }
+// }
 class Solution {
     func reverseList(_ head: ListNode?) -> ListNode? {
-        var newHead: ListNode?
-        func reverse(_ head: ListNode?) -> ListNode? {
-            guard let cur = head else { return nil }
-            if cur.next == nil {
-                newHead = cur
-            }
-            let next = reverse(cur.next)
-            next?.next = cur
-            return cur
+        func reverse(_ cur: ListNode?, _ prev: ListNode? = nil) -> ListNode? {
+            guard cur != nil else { return prev }
+            let next = cur?.next
+            cur?.next = prev
+            let newHead = reverse(next, cur)
+            return newHead
         }
-        reverse(head)?.next = nil
-
-        return newHead
+        return reverse(head)
     }
 }
