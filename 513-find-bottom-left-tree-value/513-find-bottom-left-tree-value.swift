140fd60c4d6460be2a8926d9675ad04797a8c27d
Huyanh Hoang <whoyawn@outlook.com>
2022-01-31 23:03:26 -0800

    Time: 36 ms (100.00%), Space: 15 MB (10.00%) - LeetHub

---
+++ 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.swift
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func findBottomLeftValue(_ root: TreeNode?) -> Int {
+        guard let root = root else { return 0 }
+        var currentRow: [TreeNode] = []
+        var queue = currentRow
+        queue.append(root)
+        while !queue.isEmpty {
+            currentRow = queue
+            for _ in 0..<queue.count {
+                let front = queue.removeFirst()
+                if let l = front.left {
+                    queue.append(l)
+                }
+                if let r = front.right {
+                    queue.append(r)
+                }
+            }
+        }
+        return currentRow.first?.val ?? 0
+    }
+}
