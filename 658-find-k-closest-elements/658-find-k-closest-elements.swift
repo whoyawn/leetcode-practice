5065692eaaeb16c2fb58dde9227d279cb8b42203
Huyanh Hoang <whoyawn@outlook.com>
2022-02-12 15:20:40 -0800

    Time: 298 ms (65.26%), Space: 14.8 MB (45.26%) - LeetHub

---
+++ 658-find-k-closest-elements/658-find-k-closest-elements.swift
@@ -0,0 +1,25 @@
+"""
+arr = [-10,-2,3,4,5] k = 2, x = 3 => [3,4]
+arr = [-10,-2,3,10,50] k = 2, x = 3 => [3,-2]
+
+ 2 3 4 5 6
+[1,2,3,4,5] k = 4 x = -1
+
+  1  0 2 3 5
+[-2,-1,1,2,4] k = 4 x = -1
+
+x may not be in arr
+
+algo
+1. find closest to x O(logn) heap function: closest to x (max heap)
+2. find k closest, dq k times
+3. return sorted array of k
+
+O(n + klogn)
+"""
+class Solution {
+    func findClosestElements(_ arr: [Int], _ k: Int, _ x: Int) -> [Int] {
+        var heap = arr.sorted(by: { abs($0 - x) < abs($1 - x) || (abs($0 - x) == abs($1 - x) && $0 < $1) }) // O(n) for real heapify
+        return Array(heap[0..<k]).sorted()
+    }
+}
