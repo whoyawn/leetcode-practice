0722a08c0c2ca7971b0f6e535f85f7c166d143d0
Huyanh Hoang <whoyawn@outlook.com>
2022-02-14 12:31:43 -0800

    Time: 37 ms (49.18%), Space: 14.8 MB (18.03%) - LeetHub

--- 443-string-compression/443-string-compression.swift
+++ 443-string-compression/443-string-compression.swift
@@ -1,37 +1,39 @@
 """
-maintain cur
+two pointers
+writer & reader
 
-see new char/end of array -> dump cur + (count if > 1)
+move reader, if new letter, then writer writes cur and count
 
-convert count -> 12 int -> string -> char array, iterate left to right 
+total count = reader + 1
 
+0. write char, move
+1. write count
+2. move
 """
 class Solution {
     func compress(_ chars: inout [Character]) -> Int {
         if chars.isEmpty { return 0 }
-        var s: [Character] = []
+        chars.append("¡")
+        var writer = 0
         var cur = chars[0]
         var count = 1
-        for (i, char) in chars.enumerated() where i > 0 {
-            if cur == char {
-                count += 1
-            } else {
-                s.append(cur)
+        for (reader, char) in chars.enumerated() where reader > 0 {
+            if cur != char {
+                chars[writer] = cur
+                writer += 1
                 cur = char
-                if count > 1 {
-                    let countChars = Array(String(count))
-                    s.append(contentsOf: countChars)
-                    count = 1
+                guard count > 1 else { continue }
+                let nums = Array(String(count))
+                for num in nums {
+                    chars[writer] = num
+                    writer += 1
                 }
+                count = 1
+            } else {
+                count += 1
             }
         }
-        s.append(cur)
-        if count > 1 {
-            let countChars = Array(String(count))
-            s.append(contentsOf: countChars)
-            count = 1
-        } 
-        chars = s
-        return s.count
+        
+        return writer
     }
 }
