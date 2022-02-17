61361073d0cc7ea84f7697afacac9233092094eb
Huyanh Hoang <whoyawn@outlook.com>
2022-02-17 14:53:15 -0800

    Time: 454 ms (70.00%), Space: 15.6 MB (70.00%) - LeetHub

--- 271-encode-and-decode-strings/271-encode-and-decode-strings.swift
+++ 271-encode-and-decode-strings/271-encode-and-decode-strings.swift
@@ -7,18 +7,57 @@
 2. add delimiter (certain seq of chars)
 
 O(n*m)
+
+["Hello","World"]
+5#Hello5#World
+
+["8#Hell#o","World"]
+8#8#Hello
+
+["",""]
+0#0#
+
+123#
 """
 class Codec {
     func encode(_ strs: [String]) -> String {
-        strs
-        .map { $0.replacingOccurrences(of: "#", with: "##") }
-        .joined(separator: "!#!")
+        // strs
+        // .map { $0.replacingOccurrences(of: "#", with: "##") }
+        // .joined(separator: "!#!")
+        var encoded = ""
+        for str in strs {
+            encoded += String(str.count) + "#" + str
+        }
+        return encoded
     }
-    
+    //5#Hello5#World
     func decode(_ s: String) -> [String] {
-        return s
-        .components(separatedBy: "!#!")
-        .map { $0.replacingOccurrences(of: "##", with: "#") }
+        // return s
+        // .components(separatedBy: "!#!")
+        // .map { $0.replacingOccurrences(of: "##", with: "#") }
+        var chars = Array(s)
+        var decoded: [String] = []
+        var i = 0
+        var number = 0
+        while i < chars.count {
+            if chars[i] != "#" {
+                let digit = chars[i].wholeNumberValue!
+                if digit == 0 {
+                    decoded.append("")
+                    i += 2
+                } else {
+                    while chars[i] != "#" {
+                        number *= 10
+                        number += chars[i].wholeNumberValue!
+                        i += 1
+                    }
+                    decoded.append(String(chars[i+1..<i+1+number]))
+                    i += number + 1
+                    number = 0
+                }
+            }
+        }
+        return decoded
     }
 }
 
