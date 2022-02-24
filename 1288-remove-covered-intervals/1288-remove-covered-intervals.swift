7fef6318ed5b0aabfee5202d84f54754e2c19e46
Huyanh Hoang <whoyawn@outlook.com>
2022-02-23 19:13:22 -0800

    Time: 122 ms (51.88%), Space: 14.1 MB (84.96%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.swift
@@ -0,0 +1,33 @@
+"""
+1a--3b
+1c---4d
+
+2a----8b
+ 3c--6d
+
+1a   4b
+ 2c 3d
+"""
+class Solution {
+    func removeCoveredIntervals(_ intervals: [[Int]]) -> Int {
+        var merged: [[Int]] = []
+        let sorted = intervals.sorted(by: { ($0[0], $0[1]) < ($1[0], $1[1]) })
+        print(sorted)
+        for interval in sorted {
+            if let last = merged.last {
+                let a = last[0]
+                let b = last[1]
+                let (c, d) = (interval[0], interval[1])
+                if (c <= a && b <= d) || (a <= c && d <= b) {
+                    merged.removeLast()
+                    merged.append([a, max(b, d)])
+                } else {
+                    merged.append(interval)
+                }
+            } else {
+                merged.append(interval)
+            }
+        }
+        return merged.count
+    }
+}
