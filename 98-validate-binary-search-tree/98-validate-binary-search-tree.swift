993dfe7f7d1908889d84c4efda5e7064f4c82b89
Huyanh Hoang <whoyawn@outlook.com>
2022-02-27 15:52:56 -0800

    Time: 45 ms (68.74%), Space: 14.6 MB (90.58%) - LeetHub

---
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.swift
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+"""
+inorder -> sorted
+check prev as your go, if cur <= prev then false, else true
+"""
+class Solution {
+    func isValidBST(_ root: TreeNode?) -> Bool {
+        var isValid = true
+        var inorder: [Int] = []
+        func dfs(_ root: TreeNode?) {
+            guard let root = root else { return }
+            dfs(root.left)
+            if root.val <= inorder.last ?? Int.min { 
+                isValid = false
+                return
+            }
+            inorder.append(root.val)
+            dfs(root.right)
+        }
+        dfs(root)
+        return isValid
+    }
+}
